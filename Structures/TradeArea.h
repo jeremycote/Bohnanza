35b002d097b3942dd1f7d95f647f94ec7dbafe07
Jeremy Cote <jeremymcote@gmail.com>
2022-11-07 13:58:19 -0500

    Create trade area

---
+++ Structures/TradeArea.h
@@ -0,0 +1,50 @@
+//
+// Created by Jeremy Cote on 2022-11-07.
+//
+
+#ifndef BEANS_TRADEAREA_H
+#define BEANS_TRADEAREA_H
+
+#include "list"
+
+#include "../Cards/Card.h"
+#include "../CardFactory.h"
+
+using namespace std;
+
+class TradeArea {
+    list<Card> cards;
+
+public:
+    TradeArea() {
+
+    }
+
+    TradeArea(istream&, const CardFactory*) {
+
+    }
+
+    TradeArea& operator+=(Card* card) {
+
+    }
+
+    bool legal(Card* card) {
+
+    }
+
+    Card* trade(string cardName) {
+
+    }
+
+    int numCards() {
+        return cards.size();
+    }
+
+    friend ostream& operator<< (ostream& out, const TradeArea& tradeArea);
+};
+
+ostream& operator<< (ostream& out, const TradeArea& tradeArea) {
+    return out;
+}
+
+#endif //BEANS_TRADEAREA_H
